70a0869312c303cbb963b92a4bc9b83a8a1aaf85
lqq <liquanqing110@qq.com>
2020-06-10 23:46:35 +0800

    完善接受msg

--- rtmp/rtmpserver.go
+++ rtmp/rtmpserver.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	`encoding/binary`
 	`fmt`
 	`io`
 	`net`
@@ -23,7 +24,6 @@ func Run(addr string) error {
 	}
 }
 
-
 func handlerConnect(c net.Conn) {
 	//开始握手
 	err := handshake(c)
@@ -31,48 +31,107 @@ func handlerConnect(c net.Conn) {
 		fmt.Println("handshake fail err = ", err.Error())
 	}
 	
-	RecMsg()
+	rs := RtmpSession{
+		c:c,
+	}
+	err := receiveMsg(&rs)
+	if err != nil {
+	
+	}
+	
+	//close 只作为警告处理
+	err := c.Close()
+	
+	
 }
 
 
 
-func receiveMsg(c net.Conn) error {
+func receiveMsg(s *RtmpSession) error {
 	
 	var err error
 	var fmt byte
-	
 	bootstrap := make([]byte, 11)
-	
 	for {
-		_, err := io.ReadAtLeast(c, bootstrap, 1)
+		_, err := io.ReadAtLeast(s.c, bootstrap[:1], 1)
 		if err != nil {
 			return err
 		}
 	
 		//forward 2 bit
-		fmt = bootstrap[0] & 0xc0
-		
+		fmt = (bootstrap[0] >> 6)& 0x03
 		//last 6 bit
-		switch bootstrap[0] & 0x3f {
-		//type 0-> next 1 bit
+		csid := int(bootstrap[0] & 0x3f)
+		
+		
+		//ngx的read状态比较复杂，每个work只有一个事件循环，不能有任何阻塞操作
+		//怎么做到chunk解析的，如果到对应的判断处发现buf不够。理没有读到的话，
+		//下一次还要继续恢复现场后到这个，每次都是read完后都是需要保证下一次还能到这块。
+		//ngx rtmp这块的代码可以作为一个通用的非阻塞c读取chunk的解决方案
+		
+		switch csid {
 		case 0:
 			//read 1 byte, csid
-			_, err = io.ReadAtLeast(c, bootstrap, 1)
+			if _, err = io.ReadAtLeast(s.c, bootstrap[:1], 1); err != nil {return err}
+			csid = int(bootstrap[0]) + 64
 			break
 		case 1:
+			if _, err = io.ReadAtLeast(s.c, bootstrap[:2], 2); err != nil {return err}
+			csid = int(bootstrap[0]) + int(bootstrap[1]) * 256
 			break
-		case 2:
-			break
+		//case 2:
+		//	//common?
+		//	break
 		default:
-		
+			//noop
 		}
 		
+		//将当前流的msg和head放到流中
+		s := getStream(csid)
+		s.head.csid = 1
 		
+		//开始解析Msg
+		
+		//switch fmt {
+		//case 3:
+		//case 2:
+		//}
+		if fmt <= 2 {
+		
+		
+		}
 	}
 }
 
+var csid2stream map[int]*Stream
+
+func getStream(csid int) *Stream{
+	
+	s, ok := csid2stream[csid]
+	if !ok {
+		csid2stream[csid] = &Stream{
+			head:   Header{
+				csid:csid,
+			},
+			msgBuf: make([]byte, 100),
+		}
+	}
+}
+
+type Header struct {
+	csid int
+	timestamp int64
+	msgType int
+	msgId int
+}
+
+type Stream struct {
+	head    Header
+	msgBuf []byte
+}
+
 type RtmpSession struct {
-	CSID int
+	c net.Conn
 }
 
 
